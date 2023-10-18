fabc73a16a00721e9da026dca48e0b7a7407d628
jbubu97 <jabbar.babu81@gmail.com>
2023-10-18 15:06:22 +0600

    README.md

--- script.js
+++ script.js
@@ -1,66 +1,66 @@
 // for loop
 
-// for (let i = 0; i < 10; i++) {
-//     const element = i+1;
-//     console.log(element);
-// };
+for (let i = 0; i < 10; i++) {
+    const element = i+1;
+    console.log(element);
+};
 
-// for (let i = 0; i < 10; i++) {
-//     const element = i;
-//     if (element == 5) {
-//         console.log(`${i} is my favourite number`);
-//     }
-//     console.log(element);
-// };
-
-
-// for (let i = 1; i <= 20; i++) {
-//     if (i == 5) {
-//         console.log(`my favourite number 5`);
-//         break;
-//     }
-//     console.log(`value of i is ${i}`);
-// };
-
-// for (let i = 1; i <= 20; i++) {
-//     if (i == 5) {
-//         console.log(`my favourite number 5`);
-//         continue;
-//     }
-//     console.log(`value of i is ${i}`);
-// };
+for (let i = 0; i < 10; i++) {
+    const element = i;
+    if (element == 5) {
+        console.log(`${i} is my favourite number`);
+    }
+    console.log(element);
+};
 
 
-// for (let i = 1; i <= 10; i++) {
-//     console.log(`Outer loop value ${i}`);
-//     for (let j = 1; j <= 10; j++) {
-//         // console.log(`Inner Looop value is ${j} and inner Loop in ${i}`);
-//         console.log(i + " * " + j + " = " + i*j);
-//     }
-// };
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log(`my favourite number 5`);
+        break;
+    }
+    console.log(`value of i is ${i}`);
+};
 
-// const myArray = ['Bangladesh', 'Pakistan', 'South Africa', 'New Zealand'];
+for (let i = 1; i <= 20; i++) {
+    if (i == 5) {
+        console.log(`my favourite number 5`);
+        continue;
+    }
+    console.log(`value of i is ${i}`);
+};
 
-// for (let i = 0; i < myArray.length; i++) {
-//     const element = myArray[i];
-//     console.log(element);
-// };
 
-// const myArr = ['Rahim', 'Karim', 'Jamal', 'Kamal'];
+for (let i = 1; i <= 10; i++) {
+    console.log(`Outer loop value ${i}`);
+    for (let j = 1; j <= 10; j++) {
+        // console.log(`Inner Looop value is ${j} and inner Loop in ${i}`);
+        console.log(i + " * " + j + " = " + i*j);
+    }
+};
 
-// for (let i = 0; i < myArr.length; i++) {
-//     const element = myArr[i];
-//     console.log(element);
-// }
+const myArray = ['Bangladesh', 'Pakistan', 'South Africa', 'New Zealand'];
+
+for (let i = 0; i < myArray.length; i++) {
+    const element = myArray[i];
+    console.log(element);
+};
+
+const myArr = ['Rahim', 'Karim', 'Jamal', 'Kamal'];
+
+for (let i = 0; i < myArr.length; i++) {
+    const element = myArr[i];
+    console.log(element);
+}
 
 
 // while loop
 
-// let i = 0
-// while (i <= 10) {
-//     console.log(i);
-//     i++
-// };
+let i = 0
+while (i <= 10) {
+    console.log(i);
+    i++
+};
 
 let team = ['Tamim', 'Shakib', 'Mushfiq', 'Mahmudullah', 'Mashrafi'];
 
